17b7e9bca40e5ad077ef31940dafe6fcdfe87cff
Tommy Mendez <Tommy Mendez>
2017-02-16 00:36:20 -0500

    updated superstars with songs

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,9 @@ let entrants = _.shuffle(superstars.wrestlers)
 
 function revealEntrant(entrant) {
   let selection = _.find(entrants, {'id': _.toNumber(entrant.getAttribute('value'))})
-  console.log(selection)
+  console.log(selection, $(entrant))
+  $(entrant).addClass('unavailable').removeClass('available')
+  $(entrant).find("h3").removeClass('hidden')
 }
 
 let entryCounter = 0
@@ -11,7 +13,7 @@ _.forEach(entrants,(entry) => {
   entryCounter ++
   currentRow = "#row" + row
   entry.id = entryCounter
-  $( "<div class='col-sm-2'><div class='card'  value=" + entry.id + " onClick=revealEntrant(this)><div class='card-block'><h3 class='card-title'>WRESTLE</h3></div></div></div>" ).appendTo( currentRow );
+  $( "<div class='col-sm-2'><div class='card available' value=" + entry.id + " onClick=revealEntrant(this) data-toggle='modal' data-target='#exampleModal'><div class='card-block'><h3 class='card-title hidden'>" + entry.name + "</h3></div></div></div>" ).appendTo( currentRow );
   if(entryCounter % 5 === 0){
       row ++
   }
